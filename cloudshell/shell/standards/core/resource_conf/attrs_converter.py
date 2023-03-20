febb5e9aea99d5cc256efa650d3a5b5b45d24dbc
Kyrylo Maksymenko <saklar13@gmail.com>
2023-03-20 15:09:01 +0100

    move converters to separate module

--- cloudshell/shell/standards/core/resource_conf/attrs_converter.py
+++ cloudshell/shell/standards/core/resource_conf/attrs_converter.py
@@ -1,16 +1,25 @@
 from __future__ import annotations
 
-import re
 from abc import ABC, abstractmethod
-from collections.abc import Callable, Iterator
+from collections.abc import Callable
 from enum import Enum
 from importlib import import_module
 from itertools import chain
-from typing import TYPE_CHECKING, Any, ClassVar, Collection
+from typing import TYPE_CHECKING, Any, Collection
 
 from attrs import Attribute, define, field, fields
 
 from cloudshell.shell.standards.core.namespace_type import NameSpaceType
+from cloudshell.shell.standards.core.resource_conf.attrs_converters import (
+    AbsCollectionConverter,
+    AbsConverter,
+    BoolConverter,
+    IntConverter,
+    ListConverter,
+    SetConverter,
+    StrConverter,
+    TupleConverter,
+)
 from cloudshell.shell.standards.core.resource_conf.resource_attr import RAISE, AttrMeta
 from cloudshell.shell.standards.exceptions import ResourceConfigException
 
@@ -41,19 +50,6 @@ class WithoutMeta(ResourceConfigException):
 
 
 @define
-class AttributeConvertError(ResourceConfigException):
-    name: str
-    str_type: str
-    val: str
-
-    def __str__(self) -> str:
-        return (
-            f"The resource attribute '{self.name}' should be of type "
-            f"{self.str_type} but the value '{self.val}' was provided"
-        )
-
-
-@define
 class InitializeClassError(ResourceConfigException):
     name: str
     type_: type
@@ -65,114 +61,6 @@ class InitializeClassError(ResourceConfigException):
             values = ", ".join([f"'{v.value}'" for v in self.type_])
             msg += f". Possible values are: {values}"
         return msg
-
-
-class AbsConverter(ABC):
-    type_: ClassVar[type]
-
-    def __init__(self, val: str, meta: AttrMeta):
-        self.val = val
-        self.meta = meta
-
-    @classmethod
-    def get_str_type(cls) -> str:
-        return cls.type_.__name__
-
-    @classmethod
-    def is_supported_type(cls, str_type: str) -> bool:
-        return str_type.lower() == cls.get_str_type()
-
-    @abstractmethod
-    def _convert(self) -> type_:
-        ...
-
-    def convert(self) -> type_:
-        try:
-            return self._convert()
-        except Exception as e:
-            raise AttributeConvertError(
-                self.meta.name, self.get_str_type(), self.val
-            ) from e
-
-
-class AbsCollectionConverter(AbsConverter):
-    @classmethod
-    def is_supported_type(cls, str_type: str) -> bool:
-        raise NotImplementedError
-
-    @abstractmethod
-    def get_str_child_type(self, str_type: str) -> str:
-        ...
-
-    @property
-    @abstractmethod
-    def child_values(self) -> Iterator[str]:
-        ...
-
-
-class StrConverter(AbsConverter):
-    type_: ClassVar[type] = str
-
-    def _convert(self) -> type_:
-        return self.val
-
-
-class BoolConverter(AbsConverter):
-    type_: ClassVar[type] = bool
-
-    def _convert(self) -> bool:
-        val = self.val.lower()
-        if val in {"true", "yes", "y"}:
-            result = True
-        elif val in {"false", "no", "n"}:
-            result = False
-        else:
-            raise ValueError
-        return result
-
-
-class IntConverter(AbsConverter):
-    type_: ClassVar[type] = int
-
-    def _convert(self) -> int:
-        return int(self.val)
-
-
-class CollectionConverter(AbsCollectionConverter):
-    COLLECTION_SEPARATOR_PATTERN: ClassVar[re.Pattern] = re.compile(r"[,;]")
-    type_: ClassVar[type]
-
-    @classmethod
-    def get_collection_pattern(cls):
-        return re.compile(rf"^{cls.get_str_type()}\[(\w+)]$", re.I)
-
-    def get_str_child_type(self, str_type: str) -> str:
-        pattern = self.get_collection_pattern()
-        return pattern.search(str_type).group(1)
-
-    @property
-    def child_values(self) -> list[str]:
-        return self.COLLECTION_SEPARATOR_PATTERN.split(self.val)
-
-    @classmethod
-    def is_supported_type(cls, str_type: str) -> bool:
-        pattern = cls.get_collection_pattern()
-        return bool(pattern.search(str_type))
-
-    def _convert(self) -> type_:
-        return self.type_(self.child_values)
-
-
-class ListConverter(CollectionConverter):
-    type_ = list
-
-
-class TupleConverter(CollectionConverter):
-    type_ = tuple
-
-
-class SetConverter(CollectionConverter):
-    type_ = set
 
 
 class AbsResourceAttrsConverter(ABC):
