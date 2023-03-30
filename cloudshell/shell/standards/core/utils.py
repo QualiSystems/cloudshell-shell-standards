a750e25da37f8db4c260506a01bd4c6e4c27b09a
Kyrylo Maksymenko <saklar13@gmail.com>
2023-03-30 19:28:32 +0200

    validate AutoloadResource name to be valid CS str value

--- cloudshell/shell/standards/core/utils.py
+++ cloudshell/shell/standards/core/utils.py
@@ -4,7 +4,33 @@ import functools
 import re
 from collections.abc import Iterator
 
+from attrs import define
+
+from cloudshell.shell.standards.exceptions import BaseStandardException
+
 COLLECTION_SEPARATOR_PATTERN = re.compile(r"[,;]")
+
+CS_MAX_NAME_LENGTH = 100
+SPECIAL_SYMBOLS = re.escape(".-|_[]")
+CS_ALLOWED_STR_PATTERN = re.compile(rf"^[a-zA-Z\d\s{SPECIAL_SYMBOLS}]+$")
+
+
+@define
+class InvalidStrValue(BaseStandardException):
+    value: str
+
+
+class TooLongStrValue(InvalidStrValue):
+    def __str__(self):
+        return f"Value '{self.value}' is too long. Max length is {CS_MAX_NAME_LENGTH}"
+
+
+class NotSupportedSymbols(InvalidStrValue):
+    def __str__(self):
+        return (
+            f"Value '{self.value}' contains invalid symbols."
+            f" Allowed symbols are: ASCII letters, digits, spaces, and .-|_[]"
+        )
 
 
 def attr_length_validator(max_length: int):
@@ -31,3 +57,10 @@ def attr_length_validator(max_length: int):
 
 def split_list_of_values(string: str) -> Iterator[str, None, None]:
     return filter(bool, map(str.strip, COLLECTION_SEPARATOR_PATTERN.split(string)))
+
+
+def validate_str_for_cs(value: str) -> None:
+    if len(value) > CS_MAX_NAME_LENGTH:
+        raise TooLongStrValue(value)
+    if CS_ALLOWED_STR_PATTERN.match(value) is None:
+        raise NotSupportedSymbols(value)
